6d3a7fec89e5f7d2e8418d6cb55404b61bc5525c
Viver Val Bungag <viverbungag1@gmail.com>
2024-06-14 10:25:08 +0800

    |FEATURE| added a sample data in data.sql

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO expense (description, date, amount) VALUES ('Coffee', '2024-06-14', 2.50);
+INSERT INTO expense (description, date, amount) VALUES ('Sandwich', '2024-06-15', 2.50);
+INSERT INTO expense (description, date, amount) VALUES ('Coffee', '2024-06-15', 2.50);
