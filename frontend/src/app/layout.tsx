0429b3819153b9f1f576b2ac35d100d0f87f07c8
Viver Val Bungag <viverbungag1@gmail.com>
2024-06-18 10:56:39 +0800

    |FIX| revert back to normal headers

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -9,10 +9,10 @@ import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
-// export const metadata: Metadata = {
-//   title: 'Expense Tracker',
-//   description: 'An expense tracker built with Next.js and TypeScript',
-// };
+export const metadata: Metadata = {
+  title: 'Expense Tracker',
+  description: 'An expense tracker built with Next.js and TypeScript',
+};
 
 export default function RootLayout({
   children,
@@ -20,39 +20,26 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
-      <head>
-        <title>Expense Tracker</title>
-        <meta
-          name="description"
-          content="An expense tracker built with Next.js and TypeScript"
-        />
-        <meta
-          http-equiv="Content-Security-Policy"
-          content="upgrade-insecure-requests"
-        />
-      </head>
-      <html lang="en">
-        <Providers>
-          <body className={`${inter.className} h-full`}>
-            {children}
-            <ReactQueryDevtools initialIsOpen={false} />
-            <ToastContainer
-              position="bottom-right"
-              autoClose={2000}
-              hideProgressBar
-              newestOnTop={false}
-              closeOnClick
-              rtl={false}
-              pauseOnFocusLoss={false}
-              draggable
-              pauseOnHover
-              theme="dark"
-              transition={Slide}
-            />
-          </body>
-        </Providers>
-      </html>
-    </>
+    <html lang="en">
+      <Providers>
+        <body className={`${inter.className} h-full`}>
+          {children}
+          <ReactQueryDevtools initialIsOpen={false} />
+          <ToastContainer
+            position="bottom-right"
+            autoClose={2000}
+            hideProgressBar
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss={false}
+            draggable
+            pauseOnHover
+            theme="dark"
+            transition={Slide}
+          />
+        </body>
+      </Providers>
+    </html>
   );
 }
